837354f91c69f59c8d4256543269b16e273cf7bd
dayvsonmoraes-cyber <dayvsonmoraes@gmail.com>
2025-11-12 14:27:48 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"mensagem": "API funcionando com sucesso!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
